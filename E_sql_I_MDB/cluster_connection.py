b1fec2d37b7fdc30e4df2c944f2c707b40d97428
ALeonardoHF <leonardo_heredia123@hotmail.com>
2020-11-10 17:55:06 -0700

    reemplazo carpeta

--- E_sql_I_MDB/cluster_connection.py
+++
@@ -1,9 +0,0 @@
-import pymongo
-
-client = pymongo.MongoClient("URI MongoCluster")
-db = client.COCHES
-collection = db.naves
-
-mydict = { "nombre": "Lupillo Rivera", "direccion": "C/ Menor 43" }
-
-x = collection.insert_one(mydict)
